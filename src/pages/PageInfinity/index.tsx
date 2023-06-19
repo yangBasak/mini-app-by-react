8cf4c36323656b51e366d0947a2bb7ba5fa7b5cd
yangBasak <yangBasak@gmail.com>
2023-06-20 00:29:38 +0900

    SAVE: 자잘한 설정 추가 - 스트릭모드 삭제 - 글로벌 css 추가 - 네비바 스티키 처리 - 무한스크롤 구현중

--- src/pages/PageInfinity/index.tsx
+++ src/pages/PageInfinity/index.tsx
@@ -8,18 +8,23 @@ type Album = {
   url: string;
   thumbnailUrl: string;
 };
-
+/**
+ * IntersectionObserver API
+ * https://velog.io/@elrion018/%EC%8B%A4%EB%AC%B4%EC%97%90%EC%84%9C-%EB%8A%90%EB%82%80-%EC%A0%90%EC%9D%84-%EA%B3%81%EB%93%A4%EC%9D%B8-Intersection-Observer-API-%EC%A0%95%EB%A6%AC
+ * http://blog.hyeyoonjung.com/2019/01/09/intersectionobserver-tutorial/
+ *
+ */
 const PageInfinity = () => {
   const [rawData, setRawData] = useState<Album[]>([]);
   const [listData, setListData] = useState<Album[]>([]);
   const [lastId, setLastId] = useState<number>(-1);
 
-  // 최초 한 번 실행
   useEffect(() => {
     getRawData();
   }, []);
-  // rawData 변화 감
+
   useEffect(() => {
+    if (rawData.length === 0) return;
     sliceListData();
   }, [rawData]);
 
@@ -32,18 +37,16 @@ const PageInfinity = () => {
 
   const sliceListData = () => {
     if (lastId === -1) {
-      const cutList = rawData.slice(0, 100);
-      console.log(cutList);
-      //setLastId(cutList[cutList.length - 1].id);
+      const cutList = rawData.slice(0, 10);
+      setLastId(cutList[cutList.length - 1].id);
       setListData(cutList);
+    } else {
+      const lastDataIndex = rawData.findIndex((item) => item.id === lastId);
+      const cutList = rawData.slice(lastDataIndex + 1, lastDataIndex + 100);
+      const newList = [...listData, ...cutList];
+      setLastId(cutList[cutList.length - 1].id);
+      setListData(newList);
     }
-    // else {
-    //   const lastDataIndex = rawData.findIndex((item) => item.id === lastId);
-    //   const cutList = rawData.slice(lastDataIndex + 1, lastDataIndex + 100);
-    //   const newList = [...listData, ...cutList];
-    //   setLastId(cutList[cutList.length - 1].id);
-    //   setListData(newList);
-    // }
   };
 
   return (
@@ -51,8 +54,7 @@ const PageInfinity = () => {
       {listData.map((item) => {
         return (
           <li key={item.id}>
-            d
-            <img src="" alt="" />
+            <img src={item.url} alt="image" />
           </li>
         );
       })}
